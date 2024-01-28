ecf2a7441c260fe5f19cd8791e40216d29462f1e
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-01-28 22:22:56 +0630

    :card_file_box: Mapping User Entity to database table

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,4 @@
 export * from "./common.module";
 export * from "./dto";
+export * from "./embedded";
 export * from "./util";
--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,11 +1,23 @@
+import { RegistryDates } from "common";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
   name: string;
 
+  @Column({ unique: true })
   email: string;
 
+  @Column({ unique: true })
   phone: string;
 
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
