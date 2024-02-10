6bf5ce89d8a65e04cf42e72be2080917e8646313
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-02-11 00:21:19 +0630

    :memo: Reducing boilerplate in dynamic module configuration.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,8 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import databaseConfig from "./config/database.config";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(databaseConfig)],
-      inject: [databaseConfig.KEY],
-      useFactory: (
-        databaseConfiguration: ReturnType<typeof databaseConfig>,
-      ) => ({
-        type: "postgres",
-        url: databaseConfiguration.url,
-        autoLoadEntities: true,
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forRootAsync(databaseConfig.asProvider())],
 })
 export class DatabaseModule {}
