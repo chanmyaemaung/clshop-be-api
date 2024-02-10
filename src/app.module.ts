6d10baff806f7f4f25a037f42e1daf680c5d7e3f
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-02-11 00:09:09 +0630

    :white_check_mark: Configure with environment and setup database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { CommonModule } from "./common/common.module";
+import { DatabaseModule } from "./database";
 import { UsersModule } from "./domain/users/users.module";
+import { EnvModule } from "./env";
 
 @Module({
   imports: [
     UsersModule,
     CommonModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    EnvModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/database/database.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: "postgres",
+        url: process.env.DATASOURCE_URL,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
